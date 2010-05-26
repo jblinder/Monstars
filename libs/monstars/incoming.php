5cbee6ea3731764ea0b77b857bc08e7a9e3d9742
Justin Blinder <novatheory@gmail.com>
2010-05-26 01:21:52 -0400

    Most recent changes. Won't run without inserting neccessary API Keys. Parser mostly complete.

--- libs/monstars/incoming.php
+++ libs/monstars/incoming.php
@@ -18,32 +18,4 @@ if($enemy) $json_enemy  = $parser->init($enemy, $type);
 
 echo json_encode(array_merge($json_user, $json_enemy));
 
-/*
-//setup sql statements
-
-$sql_checkuser  =  'SELECT * from user where username="'.$name.'"';
-$sql_insertuser =  'INSERT INTO user (username,energy,wins,loses) VALUES ("'.$name.'", 10, 0,0)';
-$sql_updateuser =  'UPDATE '
-echo $sql_checkuser;
-//query mysql
-$query = mysql_query($sql_checkuser);
-//super hackish way of testing if the user exists
-$count = 0;
-while ($row = mysql_fetch_assoc($query)){
-	$count = 1;
-}
-
-if($count == 0){
-	echo "user does not exist";	
-}
-else{
-	
-}
-
-//parse and return data
-$parser = new parser();
-$json   = $parser->init($name, $type);
-*/
-
-
 ?>
